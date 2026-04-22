89117756807e4db166b0d7355a482b3fd67578ba
Tiffany Wesley <tiffanywesley@apples-MacBook-Air.local>
2026-04-22 13:54:33 -0500

    Add morgan logging middleware to Express server

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (request, response) => {
-    response.send("Welcome to my capstone!")
-})
+  response.send("Welcome to the Phase.ai API");
+});
 
 app.get("/status", (request, response) => {
-    response.json({message: "Service healthy"})
-})
+  response.json({ message: "Service healthy" });
+});
 
-
-
-const server = app.listen(PORT, () => console.log(`Listening on port ${server.address().port}`));
-
+const server = app.listen(PORT, () =>
+  console.log(`Listening on port ${server.address().port}`)
+);
